ce18274261ecb877a4e88a22ed447df08b46b44d
Navid Shervani-Tabar <nshervan@qa-v100-009.crc.nd.edu>
2022-01-06 14:03:08 -0500

    ENH: improving representation network

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 
 import torch
 import torchvision
+
 import numpy as np
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 
 from torch import nn, optim
@@ -19,12 +21,12 @@ class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
 
-        dim_out = 964
+        dim_out = 10
 
         # -- embedding params
         self.cn1 = nn.Conv2d(1, 16, 7)
-        self.cn2 = nn.Conv2d(16, 32, 4)
-        self.cn3 = nn.Conv2d(32, 64, 3)
+        self.cn2 = nn.Conv2d(16, 32, 6)
+        self.cn3 = nn.Conv2d(32, 64, 5)
         self.pool = nn.MaxPool2d(2)
 
         self.bn1 = nn.BatchNorm2d(16)
@@ -32,9 +34,9 @@ class MyModel(nn.Module):
         self.bn3 = nn.BatchNorm2d(64)
 
         # -- prediction params
-        self.fc1 = nn.Linear(256, 170)
-        self.fc2 = nn.Linear(170, 50)
-        self.fc3 = nn.Linear(50, dim_out)
+        self.fc1 = nn.Linear(256, 100)
+        self.fc2 = nn.Linear(100, 200)
+        self.fc3 = nn.Linear(200, dim_out)
 
         # -- non-linearity
         self.relu = nn.ReLU()
@@ -43,17 +45,17 @@ class MyModel(nn.Module):
 
     def forward(self, x):
 
-        y1 = self.pool(self.bn1(self.relu(self.cn1(x))))
-        y2 = self.pool(self.bn2(self.relu(self.cn2(y1))))
-        y3 = self.relu(self.bn3(self.cn3(y2)))
+        x = self.relu(self.bn1(self.cn1(x)))
+        x = self.pool(x)
+        x = self.relu(self.cn2(x))
+        x = self.relu(self.cn3(x))
 
+        x = x.view(x.size(0), -1)
 
-        y3 = y3.view(y3.size(0), -1)
+        x = self.sopl(self.fc1(x))
+        x = self.sopl(self.fc2(x))
 
-        y5 = self.sopl(self.fc1(y3))
-        y6 = self.sopl(self.fc2(y5))
-
-        return self.fc3(y6)
+        return self.fc3(x)
 
 
 class Train:
@@ -63,13 +65,15 @@ class Train:
 
         # -- data
         dim = 28
-        batch_size = 400
+        batch_size = 100
         my_transforms = transforms.Compose([transforms.Resize((dim, dim)), transforms.ToTensor()])
-        trainset = torchvision.datasets.Omniglot(root="./data/omniglot_train/", download=False, transform=my_transforms)
-        validset = torchvision.datasets.Omniglot(root="./data/omniglot_train/", background=False, download=False,
-                                                     transform=my_transforms)
+
+        trainset = torchvision.datasets.FashionMNIST('./data/fashionMNIST/', train=True, download=True, transform=my_transforms)
+        validset = torchvision.datasets.FashionMNIST('./data/fashionMNIST/', train=False, download=True, transform=my_transforms)
+
         self.TrainDataset = DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True)
-        self.ValidDataset = DataLoader(dataset=validset, batch_size=len(validset), shuffle=False)
+        self.ValidDataset = DataLoader(dataset=validset, batch_size=len(validset), shuffle=True)
+
         self.N_train = len(trainset)
         self.N_valid = len(validset)
 
@@ -79,18 +83,28 @@ class Train:
         # -- train
         self.epochs = 3000
         self.loss = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2)
+
+    @staticmethod
+    def accuracy(logits, label):
+
+        pred = F.softmax(logits, dim=1).argmax(dim=1)
+
+        return torch.eq(pred, label).sum().item()
 
     def train_epoch(self):
 
         self.model.train()
         train_loss = 0
         for batch_idx, data_batch in enumerate(self.TrainDataset):
+
+            data_batch[0], data_batch[1] = data_batch[0].to(self.device), data_batch[1].to(self.device)
+
             # -- predict
-            predict = self.model(data_batch[0].to(self.device))
+            predict = self.model(data_batch[0])
 
             # -- loss
-            loss = self.loss(predict, data_batch[1].to(self.device))
+            loss = self.loss(predict, data_batch[1])
 
             # -- optimize
             self.optimizer.zero_grad()
@@ -105,21 +119,27 @@ class Train:
         with torch.no_grad():
             self.model.eval()
             for data_batch in self.ValidDataset:
+
+                data_batch[0], data_batch[1] = data_batch[0].to(self.device), data_batch[1].to(self.device)
+
                 # -- predict
-                predict = self.model(data_batch[0].to(self.device))
+                predict = self.model(data_batch[0])
 
                 # -- loss
-                loss = self.loss(predict, data_batch[1].to(self.device))
+                loss = self.loss(predict, data_batch[1])
 
-        return loss.item()
+                # -- accuracy
+                acc = self.accuracy(predict, data_batch[1]) / len(self.ValidDataset.dataset)
+        
+        return loss.item(), acc 
 
     def __call__(self):
 
         for epoch in range(self.epochs):
             train_loss = self.train_epoch()
-            valid_loss = self.valid_epoch()
+            valid_loss, acc = self.valid_epoch()
 
-            print('Epoch {}: Training loss = {:.5f}, Validation loss = {:.5f}.'.format(epoch, train_loss, valid_loss))
+            print('Epoch {}: Training loss = {:.5f}, Validation loss = {:.5f}, Validation accuracy = {:.1f}%'.format(epoch, train_loss, valid_loss, acc*100.))
 
         torch.save(self.model.state_dict(), './model_stat.pth')
 
