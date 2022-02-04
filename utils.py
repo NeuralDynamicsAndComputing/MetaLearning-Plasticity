0e46889fbab8254368f41ce8d5589efd4cdee529
Navid Shervani-Tabar <nshervan@crcfe02.crc.nd.edu>
2022-02-04 18:32:31 -0500

    MNT: updated .gitignore and plotting params

--- utils.py
+++ utils.py
@@ -33,9 +33,9 @@ def plot_meta(filename, title, y_lim, K, res_dir, data_type='.png'):
 def plot_adpt(filename, title, y_lim, K, res_dir, data_type='.png'):
     y = np.loadtxt(res_dir + '/' + filename)
     y = np.nan_to_num(y)
-    for idx in range(0, y.shape[0], 100):
+    for idx in range(0, y.shape[0], 500):
         plt.plot(np.array(range(y.shape[1])), y[idx])
-    plt.legend(range(0, y.shape[0], 100))
+    plt.legend(range(0, y.shape[0], 500))
     plt.title(title + ', $K={}$'.format(K))
     plt.ylim(y_lim)
     plt.savefig(res_dir + '/' + title + '_K' + str(K) + data_type, bbox_inches='tight')
